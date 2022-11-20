a189f6ad1f8d1f441cb95ecf4ab2fe9cdb5b5c1e
Thomas Arend <thomas@arend-rhb.de>
2022-11-20 22:51:13 +0100

    Add hostname to CSV file

--- SQL/import.sql
+++ SQL/import.sql
@@ -3,11 +3,12 @@ use Snowflake;
 drop table if exists Snowflake;
 
 create table if not exists `Snowflake` (
+  `Host` CHAR(255),
   `Zeit` DATETIME,
   `Connections` int(11) NOT NULL DEFAULT 0,
   `Upload` bigint(20) NOT NULL DEFAULT 0,
   `Download` bigint(20) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`Zeit`)
+  PRIMARY KEY (`Host`,`Zeit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 LOAD DATA LOCAL 
