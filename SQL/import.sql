e90ec83c9a1815ffd2758ae49df4839fdccd30f1
Thomas Arend <thomas@arend-rhb.de>
2022-11-20 22:09:17 +0100

    Fixed switched up- and download

--- SQL/import.sql
+++ SQL/import.sql
@@ -1,18 +1,17 @@
-use Snowfalke;
+use Snowflake;
 
-drop table if exists Snowfalke;
+drop table if exists Snowflake;
 
-create temporary table if not exists `Snowflake` (
+create table if not exists `Snowflake` (
   `Zeit` DATETIME,
   `Connections` int(11) NOT NULL DEFAULT 0,
-  `Download` bigint(20) NOT NULL DEFAULT 0,
   `Upload` bigint(20) NOT NULL DEFAULT 0,
+  `Download` bigint(20) NOT NULL DEFAULT 0,
   PRIMARY KEY (`Zeit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 LOAD DATA LOCAL 
-INFILE '/tmp/Snowflake.csv'      
+INFILE '/tmp/snowflake.csv'      
 INTO TABLE `Snowflake`
 FIELDS TERMINATED BY ';'
-IGNORE 0
-ROWS;
+IGNORE 0 ROWS;
